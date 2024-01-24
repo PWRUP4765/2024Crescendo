0ece9a2e264dd1cc5f1c5e01bb59f5e6043bb4e9
zachgalbs <zacharythomasgalbraith@gmail.com>
2024-01-23 22:36:53 -0800

    Some changes
    Added the RobotCommand.java file, and changed the intakeSubsystem file to include a sensor, with the placeholder of CANSparkMax, and set the motor like I would on the ROMY

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,33 +1,47 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix6.configs.CANcoderConfiguration;
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
-import com.ctre.phoenix6.signals.SensorDirectionValue;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-public class IntakeSubsystem {
-
+public class IntakeSubsystem extends SubsystemBase {
+    //PLACEHOLDER
+    private CANSparkMax intakeSensor;
     private CANSparkMax inputMotor;
     private CANSparkMax shooterMotor1;
     private CANSparkMax shooterMotor2;
 
     public void IntakeSubsytem(int inputMotorChannel, int sensorChannel, int shooterMotorChannel, int shooterMotorChannel2){
-
+        //PLACEHOLDER
+        intakeSensor = new CANSparkMax(sensorChannel, MotorType.kBrushed);
         inputMotor = new CANSparkMax(inputMotorChannel, MotorType.kBrushless);
         shooterMotor1 = new CANSparkMax(shooterMotorChannel, MotorType.kBrushless);
         shooterMotor2 = new CANSparkMax(shooterMotorChannel2, MotorType.kBrushless);
 
+    }
+    @Override
+    public void periodic() {
+        // Check if the intake sensor is triggered
+        if (/*intakeSensor.get()*/ true) {
+            // Start intake motor
+            inputMotor.set(1.0); // Adjust speed as necessary
+        } else {
+            // Stop intake motor
+            inputMotor.set(0.0);
+        }
+
+        // Check if the shooter sensor is triggered
+        if (/*intakeSensor.get()*/ true) {
+            // Start shooter motors
+            shooterMotor1.set(1.0); // Adjust speed as necessary
+            shooterMotor2.set(1.0); // Adjust speed as necessary
+        } else {
+            // Stop shooter motors
+            shooterMotor1.set(0.0);
+            shooterMotor2.set(0.0);
+        }
     }
 
 }
