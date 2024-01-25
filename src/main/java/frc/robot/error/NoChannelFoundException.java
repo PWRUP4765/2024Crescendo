c2a7cee03ee38a4a29729015de9848ceedc9c552
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-01-24 18:11:51 -0800

    add errors for motor subsystems

---
+++ src/main/java/frc/robot/error/NoChannelFoundException.java
@@ -0,0 +1,16 @@
+package frc.robot.error;
+
+public class NoChannelFoundException extends Exception {
+
+  public final int channelTried;
+
+  public NoChannelFoundException() {
+    super("No channel found!");
+    this.channelTried = -101;
+  }
+
+  public NoChannelFoundException(int channelTried) {
+    super("No channel found! Tried: " + channelTried);
+    this.channelTried = channelTried;
+  }
+}
