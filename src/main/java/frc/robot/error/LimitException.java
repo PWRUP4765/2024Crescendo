d03e13e970ab85780a74ee9b6a1ef5f990e2fe67
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-01-24 23:12:42 -0800

    generalise the SpeedLimitException to work for other Limit Exceptions

---
+++ src/main/java/frc/robot/error/LimitException.java
@@ -0,0 +1,15 @@
+package frc.robot.error;
+
+public class LimitException extends Exception {
+
+  final double speed;
+  final String subsystemBroken;
+
+  public LimitException(double speedTriedToSet, String subsystemName) {
+    super(
+      "The limit that you set has been broken! This is in " + subsystemName
+    );
+    this.speed = speedTriedToSet;
+    this.subsystemBroken = subsystemName;
+  }
+}
