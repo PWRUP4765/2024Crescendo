e8ca0cfd4b91c8b2a7847800e8b8c00bbd584dc7
brigerodev <62488289+lolhol@users.noreply.github.com>
2024-01-25 18:28:11 -0800

    re-code the BalanceMotorAtSpot algorithm (implement phisics formula)

--- src/main/java/frc/robot/commands/BalanceMotorAtSpot.java
+++ src/main/java/frc/robot/commands/BalanceMotorAtSpot.java
@@ -7,68 +7,73 @@ import frc.robot.util.MathFun;
 
 /**
  * @author godbrigero
+ * @apiNote this features both an adaptive method of having the arm stay / move to a place and a avg method which idk if works
  */
 public class BalanceMotorAtSpot extends Command {
 
   final BalanceMotorAtSpotInterface motorCommands;
-  final double motorSpeed, destPos, getMotorCurPosCoef, acuracy, motorMaxSpeedPerc;
-  private double last, yCoef;
+  final boolean stayAtTop;
+  final double motorSpeed, destPos, getMotorCurPosCoef, motorMaxSpeedPerc, acuracy;
+  double topStaySpeed = 101;
+  long time;
   private boolean isWorking = false;
 
   /**
-   * @param motorCommands the commands that are passed into the code so the code can control the arm
-   * @param normalSpeed the speed at which the arm will be going when it is below / above the desired location
-   * @param destPos the destination position of the return of the motorCommands.getMotorPos
-   * @param getMotorCurPosCoef if you have a circle, you will need like a conversion factor from revolutions to actual pos
-   * this is for that. If your function (getPos) returns the amt of revolutions, you will have to multiply the revolutions by
-   * the circum. (ik this is kinda false dont ask)
-   * @param acuracy the +- spot that you want the function to be at (0 is not recomended)
+   * @param motorCommands the motor commands
+   * @param normalSpeed the speed at which the motor will execute
+   * @param destPos the destination pos
+   * @param getMotorCurPosCoef since the "getPosition()" ofter returns its value in revolutions, we need to convert that into
+   * meters. This is what this value is for. The code will multiply the "getPosition()" by this value
+   * @param acuracy the +- position that the motor has to be in
+   * @param stayAtTop if true, it will TRY to stay on top. If the normal speed istoo high tho, it will just break (probably)
    */
   public BalanceMotorAtSpot(
     BalanceMotorAtSpotInterface motorCommands,
     double normalSpeed,
     double destPos,
     double getMotorCurPosCoef,
-    double acuracy
+    double acuracy,
+    boolean stayAtTop
   ) {
     this.motorCommands = motorCommands;
     this.motorSpeed = normalSpeed;
     this.destPos = destPos;
     this.getMotorCurPosCoef = getMotorCurPosCoef;
     this.motorMaxSpeedPerc = 0;
-    this.acuracy = acuracy;
+    this.time = 0;
+    this.stayAtTop = stayAtTop;
+    this.acuracy = 0;
   }
 
   /**
-   * @param motorCommands the motor commands and outputs
-   * @param destPos the destionation position of the motor
-   * @param motorMaxSpeedPerc since this is an adaptive method of moving the motor to position, it can have a max speed. In %.
-   * @param getMotorCurPosCoef curPosCoef see above
-   * @param acuracy +- where the motor should be.
-   * @throws LimitException if the motorMaxSpeed is wrong
+   * @param motorCommands the motor comands such as "setSpeed()"
+   * @param destPos the destination position in Meters
+   * @param getMotorCurPosCoef since the "getPosition()" ofter returns its value in revolutions, we need to convert that into
+   * meters. This is what this value is for. The code will multiply the "getPosition()" by this value
+   * @param time the time it will take the arm to get to the spot
+   * @param stayAtTop if true will stay at top. if not, will just turn off the motor
    */
   public BalanceMotorAtSpot(
     BalanceMotorAtSpotInterface motorCommands,
     double destPos,
-    float motorMaxSpeedPerc,
     double getMotorCurPosCoef,
-    double acuracy
-  ) throws LimitException {
-    if (
-      motorMaxSpeedPerc > 100 || motorMaxSpeedPerc < 0
-    ) throw new LimitException(motorMaxSpeedPerc, this.getClass().getName());
+    long time,
+    boolean stayAtTop
+  ) {
     this.motorCommands = motorCommands;
     this.motorSpeed = -1;
     this.destPos = destPos;
     this.getMotorCurPosCoef = getMotorCurPosCoef;
-    this.motorMaxSpeedPerc = motorMaxSpeedPerc;
-    this.acuracy = acuracy;
+    this.motorMaxSpeedPerc = 0;
+    this.time = time;
+    this.stayAtTop = stayAtTop;
+    this.acuracy = 0;
   }
 
   @Override
   public void initialize() {
     try {
-      motorCommands.setMotorSpeed(0);
+      motorCommands.setMotorSpeed(100);
     } catch (LimitException e) {
       e.printStackTrace();
     }
@@ -82,39 +87,47 @@ public class BalanceMotorAtSpot extends Command {
       double p = motorCommands.getMotorCurPosition() * getMotorCurPosCoef;
       if (motorSpeed != -1) {
         motorCommands.setMotorSpeed(p < destPos ? motorSpeed : -motorSpeed);
+
+        if (!stayAtTop && Math.abs(Math.abs(p) - Math.abs(destPos)) < acuracy) {
+          isWorking = false;
+        }
+
         return;
       }
 
-      if (last == 0) {
-        last = p;
-        return;
+      if (topStaySpeed != 101) {
+        if (stayAtTop) {
+          motorCommands.setMotorSpeed(topStaySpeed);
+          return;
+        } else {
+          this.isWorking = false;
+        }
       }
 
-      // Since the speed of the motor is on the graph, the speed will start very fast and then get slower as time passes
-      // we need change because motors have threshhold max speed.
-      double change = p - last;
-      double x = destPos - p;
-      // This if statement adjusts for window
-      if (p < destPos - acuracy && p < destPos + acuracy) {
-        // YCoef is used to adapt to the motor not having enough power to move up even though it is not in position.
-        // Might need re-coding
-        yCoef += change <= 0 ? 1 : 0;
-        double motorSpeed = MathFun.getPosOnGraph(x, yCoef) * 100;
-        if (motorSpeed < motorMaxSpeedPerc) {
-          motorCommands.setMotorSpeed(motorSpeed);
-        } else {
-          // TODO: this might have to be re-coded THIS IS JST FOR DEBUG!
-          throw new LimitException(motorSpeed, this.getClass().getName());
-        }
-      } else if (p > destPos + acuracy && p > destPos - acuracy) {
-        yCoef -= change >= 0 ? 1 : 0;
-        double motorSpeed = MathFun.getPosOnGraph(x, yCoef) * 100;
-        if (motorSpeed < motorMaxSpeedPerc) {
-          motorCommands.setMotorSpeed(motorSpeed);
-        } else {
-          // TODO: this might have to be re-coded THIS IS JST FOR DEBUG!
-          throw new LimitException(motorSpeed, this.getClass().getName());
-        }
+      double curMotorSpeed = motorCommands.getCurMotorSpeedPerc();
+      boolean isDown = destPos - p < 0;
+
+      double neededChange = getAccelerationChangeNeeded(
+        time,
+        isDown ? p : destPos,
+        isDown ? destPos : p,
+        curMotorSpeed
+      );
+
+      if (
+        motorMaxSpeedPerc > curMotorSpeed && -motorMaxSpeedPerc < curMotorSpeed
+      ) {
+        motorCommands.setMotorSpeed(curMotorSpeed - neededChange);
+      } else {
+        throw new LimitException(curMotorSpeed, this.getClass().getName());
+      }
+
+      time -= 0.02;
+
+      if (time <= 0) {
+        // TODO: this MIGHT need to be re-coded. Because the top speed might not be the best speed that makes the thing stay
+        // in one place ngl this needs testing.
+        topStaySpeed = curMotorSpeed;
       }
     } catch (LimitException e) {
       e.printStackTrace();
@@ -134,5 +147,23 @@ public class BalanceMotorAtSpot extends Command {
   @Override
   public boolean isFinished() {
     return !isWorking;
+  }
+
+  /**
+   * @param time time to excecute
+   * @param positionEnd end position of the "throw" in Meters
+   * @param positionStart start pos (ex - 0) in Meters
+   * @param currentSpeedPerc current speed
+   * @return will return the number by which you will have to slow down ur sleed (-=) for the motor to reach the target
+   */
+  private double getAccelerationChangeNeeded(
+    long time,
+    double positionEnd,
+    double positionStart,
+    double currentSpeedPerc
+  ) {
+    return (
+      (2 * positionEnd - positionStart - currentSpeedPerc * time) / time * time
+    );
   }
 }
